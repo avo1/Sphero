b48a572d0092e43ffb2c293b9ee2d467187cba42
avo1 <voanhdung@gmail.com>
2016-12-04 00:12:54 +0700

    add UI

--- CoDrone/ConnectViewController.swift
+++ CoDrone/ConnectViewController.swift
@@ -12,9 +12,15 @@ class ConnectViewController: UIViewController {
     
     @IBOutlet weak var deviceLabel: UILabel!
     @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var flightButton: UIButton!
+    
+    var isFlying = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Start the Bluetooth discovery process
+        _ = btDiscoverySharedInstance
         
         // Watch Bluetooth connection
         NotificationCenter.default.addObserver(self, selector: #selector(ConnectViewController.connectionChanged(_:)), name: NSNotification.Name(rawValue: BLEServiceChangedStatusNotification), object: nil)
@@ -23,6 +29,15 @@ class ConnectViewController: UIViewController {
     
     deinit {
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name(rawValue: BLEServiceChangedStatusNotification), object: nil)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        SwiftSpinner.show(delay: 0.5, title: "Shouldn't see this", animated: false)
+        SwiftSpinner.hide()
+        
+        SwiftSpinner.show(delay: 0.7, title: "Searching for drone", animated: true)
     }
     
     func connectionChanged(_ notification: Notification) {
@@ -35,7 +50,7 @@ class ConnectViewController: UIViewController {
                 if isConnected {
                     self.deviceLabel.text = btDiscoverySharedInstance.deviceName
                     self.statusLabel.text = "Drone is connected"
-                    
+                    SwiftSpinner.hide()
                 } else {
                     self.statusLabel.text = "Drone is disconnected"
                 }
@@ -44,27 +59,38 @@ class ConnectViewController: UIViewController {
     }
     
     @IBAction func scanPeripheral(_ sender: UIButton) {
-        // Start the Bluetooth discovery process
-        _ = btDiscoverySharedInstance
+        
     }
     
-
-    @IBAction func takeOff(_ sender: UIButton) {
-        print("send command: take off")
-        if let bleService = btDiscoverySharedInstance.bleService {
-            bleService.writeCommand([0x11, 0x22, 0x01])
-            statusLabel.text = "Take off"
-        }
-        //writeCommand()  //u8: 0x22: Flight Event, then 0x01: Takeoff
-    }
-    
-    @IBAction func landing(_ sender: UIButton) {
-        print("send command: landing")
+    @IBAction func estop(_ sender: UIButton) {
+        print("send command: estop")
         if let bleService = btDiscoverySharedInstance.bleService {
             bleService.writeCommand([0x11, 0x22, 0x06])
-            statusLabel.text = "Landing"
+            statusLabel.text = "E-stop"
+            isFlying = false
+            flightButton.setImage(UIImage(named: "takeoff"), for: UIControlState.normal)
         }
-        //writeCommand([0x11, 0x22, 0x06])  //u8: 0x22: Flight Event, then 0x06: Landing
+    }
+
+    @IBAction func landingOrTakeoff(_ sender: UIButton) {
+        if isFlying {
+            print("send command: landing")
+            if let bleService = btDiscoverySharedInstance.bleService {
+                bleService.writeCommand([0x11, 0x22, 0x07])
+                statusLabel.text = "Landing"
+                isFlying = false
+                flightButton.setImage(UIImage(named: "takeoff"), for: UIControlState.normal)
+            }
+            
+        } else {
+            print("send command: take off")
+            if let bleService = btDiscoverySharedInstance.bleService {
+                bleService.writeCommand([0x11, 0x22, 0x01])
+                statusLabel.text = "Take off"
+                isFlying = true
+                flightButton.setImage(UIImage(named: "landing"), for: UIControlState.normal)
+            }
+        }
     }
     
 }
